44f59567d508a58d7e35c9387bb313fb1b7cdd3c
directormac <markasena@gmail.com>
2023-06-06 09:44:14 +0800

    remove hardhacker

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,20 +13,20 @@ return {
       },
     },
   },
-  {
-    "hardhackerlabs/theme-vim",
-    name = "hardhacker",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.hardhacker_hide_tilde = 1
-      vim.g.hardhacker_keyword_italic = 1
-      vim.g.hardhacker_transparent_background = true
-      -- custom highlights
-      vim.g.hardhacker_custom_highlights = {}
-      vim.cmd("colorscheme hardhacker")
-    end,
-  },
+  -- {
+  --   "hardhackerlabs/theme-vim",
+  --   name = "hardhacker",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.hardhacker_hide_tilde = 1
+  --     vim.g.hardhacker_keyword_italic = 1
+  --     vim.g.hardhacker_transparent_background = true
+  --     -- custom highlights
+  --     vim.g.hardhacker_custom_highlights = {}
+  --     vim.cmd("colorscheme hardhacker")
+  --   end,
+  -- },
 
   {
     "LazyVim/LazyVim",
