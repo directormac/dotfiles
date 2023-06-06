0abbd9f07d5b3f9b064d3296ae555dda80000336
directormac <markasena@gmail.com>
2023-06-06 09:33:08 +0800

    hardhacker

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,11 +13,25 @@ return {
       },
     },
   },
+  {
+    "hardhackerlabs/theme-vim",
+    name = "hardhacker",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.hardhacker_hide_tilde = 1
+      vim.g.hardhacker_keyword_italic = 1
+      vim.g.hardhacker_transparent_background = true
+      -- custom highlights
+      vim.g.hardhacker_custom_highlights = {}
+      vim.cmd("colorscheme hardhacker")
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "hardhacker",
     },
   },
 }
