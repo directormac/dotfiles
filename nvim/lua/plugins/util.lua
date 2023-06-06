c50961b84650cdd069f944b32336c3069f6f64b3
directormac <markasena@gmail.com>
2023-06-06 23:54:22 +0800

    fix oil.nvim

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -6,7 +6,24 @@
 return {
   {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+      use_default_keymaps = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "none",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+      },
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
