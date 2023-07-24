22b465d963c0dc866c19908690d0305e22003f4e
directormac <markasena@gmail.com>
2023-07-25 06:23:46 +0800

    Fuck microsoft

--- nvimone/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.lazy")
-require("config.keymaps")
-require("config.options")
-require("config.autocmds")
--- Extensions Disable if necessary
-require("plugins.lsp")
---
+++ vscodenvim/lua/config/util.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.map = function(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+return M
