6ae2746f9d3d810b7c6ba41cfebae8bbab357595
Mark Asena <mac@mkra.dev>
2024-12-30 03:36:19 +0800

    remove bug

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,45 +1,42 @@
 { pkgs, ... }:
 let
-  # treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-  #   # p.ansible
-  #   p.bash
-  #   p.comment
-  #   p.css
-  #   p.dockerfile
-  #   p.fish
-  #   p.gitattributes
-  #   p.gitignore
-  #   p.go
-  #   p.gomod
-  #   p.gowork
-  #   p.hcl
-  #   p.javascript
-  #   p.jq
-  #   p.json5
-  #   p.json
-  #   p.lua
-  #   p.make
-  #   p.markdown
-  #   p.nix
-  #   p.python
-  #   p.svelte
-  #   p.rust
-  #   p.toml
-  #   p.svelte
-  #   p.typescript
-  #   p.vue
-  #   p.yaml
-  # ]));
-  #
-  # treesitter-parsers = pkgs.symlinkJoin {
-  #   name = "treesitter-parsers";
-  #   paths = treesitterWithGrammars.dependencies;
-  # };
+  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+    p.bash
+    p.comment
+    p.css
+    p.dockerfile
+    p.fish
+    p.gitattributes
+    p.gitignore
+    p.go
+    p.gomod
+    p.gowork
+    p.hcl
+    p.javascript
+    p.jq
+    p.json5
+    p.json
+    p.lua
+    p.make
+    p.markdown
+    p.nix
+    p.python
+    p.svelte
+    p.rust
+    p.toml
+    p.svelte
+    p.typescript
+    p.vue
+    p.yaml
+  ]));
+
+  treesitter-parsers = pkgs.symlinkJoin {
+    name = "treesitter-parsers";
+    paths = treesitterWithGrammars.dependencies;
+  };
 in {
   home.packages = with pkgs; [
     # Language Servers and Tools
-    ansible-language-server
-    ansible-lint
     astro-language-server
     biome
     clang-tools
@@ -67,39 +64,37 @@ in {
     vue-language-server
 
     # Themes
-    vimPlugins.catppuccin-nvim
-    vimPlugins.tokyonight-nvim
+    #vimPlugins.catppuccin-nvim
+    #vimPlugins.tokyonight-nvim
   ];
 
   programs.neovim = {
     enable = true;
-    package = pkgs.neovim;
     viAlias = true;
     vimAlias = true;
-    # coc.enable = false;
-    # withNodeJs = true;
+    withNodeJs = true;
 
-    # plugins = [ treesitterWithGrammars ];
+    plugins = [ treesitterWithGrammars ];
   };
 
-  # home.file."./.config/nvim/" = {
-  #   source = ./nvim;
-  #   recursive = true;
-  # };
-  #
-  # home.file."./.config/nvim/lua/config/init.lua".text = ''
-  #   require("config.options")
-  #   require("config.keymaps")
-  #   require("config.autocmds")
-  #   require("config.util")
-  #   vim.opt.runtimepath:append("${treesitter-parsers}")
-  # '';
-  #
-  # # Treesitter is configured as a locally developed module in lazy.nvim
-  # # we hardcode a symlink here so that we can refer to it in our lazy config
-  # home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
-  #   recursive = true;
-  #   source = treesitterWithGrammars;
-  # };
+  home.file."./.config/nvim/" = {
+    source = ./nvim;
+    recursive = true;
+  };
+
+  home.file."./.config/nvim/lua/config/init.lua".text = ''
+    require("config.options")
+    require("config.keymaps")
+    require("config.autocmds")
+    require("config.util")
+    vim.opt.runtimepath:append("${treesitter-parsers}")
+  '';
+
+  # Treesitter is configured as a locally developed module in lazy.nvim
+  # we hardcode a symlink here so that we can refer to it in our lazy config
+  home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
+    recursive = true;
+    source = treesitterWithGrammars;
+  };
 }
 
