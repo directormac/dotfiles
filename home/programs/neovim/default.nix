4fafa9e3cdf643db147a14dc3fc252af2e5ed543
Mark Asena <mac@mkra.dev>
2024-12-30 03:22:18 +0800

    bare bones

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,40 +1,40 @@
 { pkgs, ... }:
 let
-  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    # p.ansible
-    p.bash
-    p.comment
-    p.css
-    p.dockerfile
-    p.fish
-    p.gitattributes
-    p.gitignore
-    p.go
-    p.gomod
-    p.gowork
-    p.hcl
-    p.javascript
-    p.jq
-    p.json5
-    p.json
-    p.lua
-    p.make
-    p.markdown
-    p.nix
-    p.python
-    p.svelte
-    p.rust
-    p.toml
-    p.svelte
-    p.typescript
-    p.vue
-    p.yaml
-  ]));
-
-  treesitter-parsers = pkgs.symlinkJoin {
-    name = "treesitter-parsers";
-    paths = treesitterWithGrammars.dependencies;
-  };
+  # treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+  #   # p.ansible
+  #   p.bash
+  #   p.comment
+  #   p.css
+  #   p.dockerfile
+  #   p.fish
+  #   p.gitattributes
+  #   p.gitignore
+  #   p.go
+  #   p.gomod
+  #   p.gowork
+  #   p.hcl
+  #   p.javascript
+  #   p.jq
+  #   p.json5
+  #   p.json
+  #   p.lua
+  #   p.make
+  #   p.markdown
+  #   p.nix
+  #   p.python
+  #   p.svelte
+  #   p.rust
+  #   p.toml
+  #   p.svelte
+  #   p.typescript
+  #   p.vue
+  #   p.yaml
+  # ]));
+  #
+  # treesitter-parsers = pkgs.symlinkJoin {
+  #   name = "treesitter-parsers";
+  #   paths = treesitterWithGrammars.dependencies;
+  # };
 in {
   home.packages = with pkgs; [
     # Language Servers and Tools
@@ -76,10 +76,10 @@ in {
     package = pkgs.neovim;
     viAlias = true;
     vimAlias = true;
-    coc.enable = false;
-    withNodeJs = true;
+    # coc.enable = false;
+    # withNodeJs = true;
 
-    plugins = [ treesitterWithGrammars ];
+    # plugins = [ treesitterWithGrammars ];
   };
 
   # home.file."./.config/nvim/" = {
