dd8f89cc41ce7929d45b20e8cf0aae8a9eecb081
Mark Asena <mac@mkra.dev>
2024-12-30 02:05:15 +0800

    let home manager manage tmux conf

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -11,10 +11,6 @@
       baseIndex = 1;
       terminal = "tmux-256color";
       shell = "${pkgs.zsh}/bin/zsh";
-
-      # extraConfig = ''
-      #   source-file ~/.tmux.conf
-      # '';
     };
   };
 
