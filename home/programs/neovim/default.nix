0f228e7a914df5bb16f7389eca959327e54781d9
Mark Asena <mac@mkra.dev>
2024-12-30 03:44:25 +0800

    try lazy

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -82,11 +82,43 @@ in {
     recursive = true;
   };
 
-  home.file."./.config/nvim/lua/config/init.lua".text = ''
-    require("config.options")
-    require("config.keymaps")
-    require("config.autocmds")
-    require("config.util")
+  home.file."./.config/nvim/lua/config/lazy.lua".text = ''
+    -- Bootstrap lazy.nvim
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+      local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+      local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+      if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+          { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+          { out, "WarningMsg" },
+          { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+      end
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    -- Make sure to setup `mapleader` and `maplocalleader` before
+    -- loading lazy.nvim so that mappings are correct.
+    -- This is also a good place to setup other settings (vim.opt)
+    vim.g.mapleader = " "
+    vim.g.maplocalleader = "\\"
+
+    -- Setup lazy.nvim
+    require("lazy").setup({
+      spec = {
+        -- import your plugins
+        { import = "plugins" },
+      },
+      -- Configure any other settings here. See the documentation for more details.
+      -- colorscheme that will be used when installing plugins.
+      install = { colorscheme = { "catppuccin" } },
+      -- automatically check for plugin updates
+      checker = { enabled = true },
+    })
+
     vim.opt.runtimepath:append("${treesitter-parsers}")
   '';
 
