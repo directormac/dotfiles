5637c1b7ef1c1ad65fe66657141b238374dcb7b5
Mark Asena <mac@mkra.dev>
2025-01-08 07:59:09 +0800

    conditionally install mason

--- .config/mac-nvim/lua/plugins/mason.lua
+++ .config/mac-nvim/lua/plugins/mason.lua
@@ -1,5 +1,10 @@
-return {
-	{
-		"williamboman/mason.nvim",
-	},
-}
+local M = {}
+local is_nixos = vim.fn.system("uname -r") == 0
+
+if not is_nixos then
+	M = {
+		{ "williamboman/mason.nvim", opts = {} },
+	}
+end
+
+return M
