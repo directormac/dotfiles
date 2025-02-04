660d9f3405993937fef4d7e0989a15000c99baff
Mark Asena <mac@mkra.dev>
2025-02-04 15:06:30 +0800

    conform options

--- .config/mac-nvim/lua/plugins/conform.lua
+++ .config/mac-nvim/lua/plugins/conform.lua
@@ -15,5 +15,19 @@ return {
 			},
 		},
 		opts = {},
+		-- opts = function(_, opts)
+		-- 	opts.formatters_by_ft = opts.formatters_by_ft or {}
+		-- 	for _, ft in ipairs(supported) do
+		-- 		opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+		-- 		table.insert(opts.formatters_by_ft[ft], "prettier")
+		-- 	end
+		--
+		-- 	opts.formatters = opts.formatters or {}
+		-- 	opts.formatters.prettier = {
+		-- 		condition = function(_, ctx)
+		-- 			return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
+		-- 		end,
+		-- 	}
+		-- end,
 	},
 }
