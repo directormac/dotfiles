2b45efd04ca176d29a0041ba34ff4b7d5a1d0d4a
Mark Asena <mac@mkra.dev>
2026-09-07 10:50:58 +0800

    add spaces to flake.nix

--- flake.nix
+++ flake.nix
@@ -9,13 +9,18 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+
     import-tree.url = "github:vic/import-tree";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
     noctalia.url = "github:noctalia-dev/noctalia";
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
@@ -23,6 +28,7 @@
         home-manager.follows = "home-manager";
       };
     };
+
     lazyvim.url = "github:pfassina/lazyvim-nix";
   };
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
