a14119524a3f7858d68e9266a1c6dd6921b078e6
Mark Asena <mac@mkra.dev>
2026-09-08 22:46:03 +0800

    chore(nixos): lvim stylua

--- config/lvim/lua/plugins/snacks.lua
+++ config/lvim/lua/plugins/snacks.lua
@@ -1,59 +1,59 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
-		---@type snacks.Config
-		opts = {
-			indent = { enabled = true },
-			input = { enabled = true },
-			notifier = {
-				enabled = true,
-				timeout = 3000,
-			},
-			quickfile = { enabled = true },
-			scroll = { enabled = true },
-			statuscolumn = { enabled = true },
-			words = { enabled = true },
-			picker = {
-				sources = {
-					files = {
-						hidden = true, -- Show dotfiles like .config
-						ignored = false, -- Respect .gitignore (skips node_modules, etc.)
-						exclude = {
-							".git",
-							"node_modules",
-							".svelte-kit",
-							".astro",
-							".next",
-							".vercel",
-							".turbo",
-							".expert",
-							".elixir_ls",
-							".vscode",
-						},
-					},
-					grep = {
-						hidden = true,
-						ignored = false,
-						exclude = {
-							".git",
-							"node_modules",
-							".svelte-kit",
-							".astro",
-							".next",
-							".vercel",
-							".turbo",
-							".expert",
-							".elixir_ls",
-							".vscode",
-						},
-					},
-				},
-			},
-			dashboard = {
-				preset = {
-					header = [[
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = {
+        enabled = true,
+        timeout = 3000,
+      },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      picker = {
+        sources = {
+          files = {
+            hidden = true, -- Show dotfiles like .config
+            ignored = false, -- Respect .gitignore (skips node_modules, etc.)
+            exclude = {
+              '.git',
+              'node_modules',
+              '.svelte-kit',
+              '.astro',
+              '.next',
+              '.vercel',
+              '.turbo',
+              '.expert',
+              '.elixir_ls',
+              '.vscode',
+            },
+          },
+          grep = {
+            hidden = true,
+            ignored = false,
+            exclude = {
+              '.git',
+              'node_modules',
+              '.svelte-kit',
+              '.astro',
+              '.next',
+              '.vercel',
+              '.turbo',
+              '.expert',
+              '.elixir_ls',
+              '.vscode',
+            },
+          },
+        },
+      },
+      dashboard = {
+        preset = {
+          header = [[
  █████╗ ██████╗ ████████╗██╗███████╗███████╗██╗  ██╗
 ██╔══██╗██╔══██╗╚══██╔══╝██║██╔════╝██╔════╝╚██╗██╔╝
 ███████║██████╔╝   ██║   ██║█████╗  █████╗   ╚███╔╝
@@ -75,36 +75,62 @@ return {
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
             },
-				},
-			},
-		},
+        },
+      },
+    },
 
-    -- stylua: ignore
     keys = {
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
-    { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-    { "<leader><space>", function() Snacks.picker.files() end, desc = "Find Files" },
-    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
-    { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
-    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+      { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
+      { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
+      { '<leader>dps', function() Snacks.profiler.scratch() end, desc = 'Profiler Scratch Buffer' },
+      { '<leader>,', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      {
+        '<leader>/',
+        function()
+          Snacks.picker.grep({
+            layout = { hidden = { 'preview' } },
+            hidden = true,
+            ignored = true,
+            formatters = { file = { truncate = 100 } },
+          })
+        end,
+        desc = 'Grep',
+      },
+      { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
+      {
+        '<leader><space>',
+        function()
+          Snacks.picker.smart({
+            title = 'Smart File Picker',
+            header = 'Hello',
+            -- prompt = '',
+
+            layout = { hidden = { 'preview' } },
+            multi = { 'recent', 'files' },
+            hidden = true,
+            ignored = true,
+            formatters = { file = { truncate = 100 } },
+          })
+        end,
+        desc = 'Smart picker',
+      },
+      { '<leader>z', function() Snacks.zen() end, desc = 'Toggle Zen Mode' },
+      { '<leader>Z', function() Snacks.zen.zoom() end, desc = 'Toggle Zoom' },
+      { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
+      { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
+      { '<leader>n', function() Snacks.notifier.show_history() end, desc = 'Notification History' },
+      { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
+      { '<leader>cR', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
+      { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
+      { '<leader>gb', function() Snacks.git.blame_line() end, desc = 'Git Blame Line' },
+      { '<leader>gf', function() Snacks.lazygit.log_file() end, desc = 'Lazygit Current File History' },
+      { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
+      { '<leader>gl', function() Snacks.lazygit.log() end, desc = 'Lazygit Log (cwd)' },
+      { '<leader>un', function() Snacks.notifier.hide() end, desc = 'Dismiss All Notifications' },
+      { '<c-/>', function() Snacks.terminal() end, desc = 'Toggle Terminal' },
+      { '<c-_>', function() Snacks.terminal() end, desc = 'which_key_ignore' },
+      { ']]', function() Snacks.words.jump(vim.v.count1) end, desc = 'Next Reference', mode = { 'n', 't' } },
+      { '[[', function() Snacks.words.jump(-vim.v.count1) end, desc = 'Prev Reference', mode = { 'n', 't' } },
+    },
   },
-	},
 }
