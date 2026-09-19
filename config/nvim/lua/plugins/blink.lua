f074cf275530d70f86af3671a32c8bd59855535e
Mark Asena <mac@mkra.dev>
2026-09-19 15:19:45 +0800

    fix: winbox package

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -3,6 +3,9 @@ return {
     "saghen/blink.cmp",
     dependencies = { "saghen/blink.lib", "rafamadriz/friendly-snippets" }, -- Ensure it's marked as a dependency
     version = "*", -- Or your preferred version/branch
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       -- Your blink configuration here
     },
@@ -14,5 +17,16 @@ return {
     -- Then re-run the build via
     --   :Lazy build blink.cmp  See
     --   :h blink-cmp-installation  for more information.
+  },
+
+  {
+    "saghen/blink.cmp",
+    -- ft = { "oil" },
+    -- enabled = false,
+    opts = {
+      enabled = function()
+        return vim.bo.filetype ~= "oil" and vim.bo.buftype ~= "prompt" and vim.b.completion ~= false
+      end,
+    },
   },
 }
