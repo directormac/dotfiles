ab04b56db075ccb93a810e98d89226f0926e611c
Mark Asena <mac@mkra.dev>
2026-09-24 17:23:12 +0800

    feat(fallback): flake parts

--- _fallback/flake.nix
+++ _fallback/flake.nix
@@ -6,13 +6,36 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    systems = [
+      "x86_64-linux"
+      # "aarch64-linux"
+      # "x86_64-darwin"
+      # "aarch64-darwin"
+    ];
+
+    # The target here is 
+    # `nixosConfigurations.HOSTNAME` which the configuration applies to in
+    # hostname of configuration.nix
+    flake.nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
       ];
     };
+
+    perSystem = { pkgs, ... }: {
+
+      packages.default = pkgs.vim;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          vim
+        ];
+      };
+
+    };
   };
+
 }
